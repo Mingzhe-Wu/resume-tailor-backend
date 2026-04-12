c229b276f3974583c3cefbe90ec58b5afc5c8bb0
Mingzhe Wu <wmz2002914@hotmail.com>
2026-04-11 17:12:23 -0700

    init: spring boot project setup

---
+++ src/main/java/com/mingzhe/resumetailor/ResumetailorApplication.java
@@ -0,0 +1,13 @@
+package com.mingzhe.resumetailor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumetailorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumetailorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mingzhe/resumetailor/ResumetailorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mingzhe.resumetailor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumetailorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
